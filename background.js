f514e340d2af245099dfd1ef8ddd05be4cad72f2
Tom Griffey <tgriffey9389@gmail.com>
2017-04-24 22:43:42 -0700

    Initial commit, first draft of extension.

--- background.js
+++ background.js
@@ -1,37 +1,110 @@
-console.log("hello");
+var fb_visit_count = 0;
+var gm_visit_count = 0;
 
-var results = [];
+var fb_tab = 0;
+var gm_tab = 0;
 
-// Receive results from popup.js
-// Store in variable - persistent until browser window is closed!
+var start = new Date();
+start.setHours(6,0,0,0);
 
-chrome.runtime.onConnect.addListener(function(port) {
-  console.log(port.name);
-  port.onMessage.addListener(function(msg) {
-    console.log(msg.result.code);
-    timeOfSearch = new Date();
-    results.push({time: timeOfSearch, content: msg})
-    console.log(results);
-  });
+Array.prototype.contains = function(v) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] === v) return true;
+    }
+    return false;
+};
+
+Array.prototype.unique = function() {
+    var arr = [];
+    for(var i = 0; i < this.length; i++) {
+        if(!arr.contains(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr; 
+}
+
+function tally(results) {
+    var facebook_results = [];
+    var gmail_results = [];
+    var fb_sortable = [];
+    var gm_sortable = [];
+
+    for (var i = 0; i < results.length; i++) {
+        var res = results[i];
+        if (res.url.includes('facebook.com') && res.lastVisitTime >= start) {
+            facebook_results.push(res.lastVisitTime);
+        } 
+        else if (res.url.includes('mail.google.com') && res.lastVisitTime >= start) {
+            gmail_results.push(res.lastVisitTime);
+        }
+    };
+    
+    facebook_results = facebook_results.unique().sort();
+    fb_visit_count = facebook_results.length;
+    // console.log(facebook_results);
+
+    gmail_results = gmail_results.unique().sort();
+    gm_visit_count = gmail_results.length;
+    // console.log(gmail_results);
+
+    // log results
+    count_totals();
+}
+
+
+function check_totals (history_item) {
+    chrome.history.search({
+        text:'',
+        startTime: start.getMilliseconds(),
+        maxResults: 1000
+    },tally);
+}
+
+chrome.history.onVisited.addListener(check_totals);
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
+        checkTabUrl(tab.url);
+    });
 });
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+    chrome.tabs.query({'active': true}, function (activeTabs) {
+        var activeTab = activeTabs[0];
 
-// yandex codes:
+        if (activeTab == updatedTab) {
+            checkTabUrl(activeTab.url);
+        }
+    });
+});
 
-// Value   Description
-// 200 
-// Operation completed successfully
-// 401 
-// Invalid API key
-// 402 
-// Blocked API key
-// 403 
-// Exceeded the daily limit on the number of requests
-// 404 
-// Exceeded the daily limit on the amount of translated text
-// 413 
-// Exceeded the maximum text size
-// 422 
-// The text cannot be translated
-// 501 
-// The specified translation direction is not supported
+function checkTabUrl(url) {
+    if (url.includes('facebook.com')) {
+        fb_tab += 1;
+    } 
+    else if (url.includes('mail.google.com')) {
+        gm_tab += 1;
+    }
+
+    // log results
+    count_totals();
+}
+
+// check initial totals when ext is turned on
+check_totals(null);
+
+function count_totals() {
+    console.log("FB visits: ",fb_visit_count);
+    console.log("Gmail visits: ",gm_visit_count);
+    console.log("FB tabbed to: ",fb_tab);
+    console.log("Gmail tabbed to: ",gm_tab);
+    
+    if ((fb_visit_count + fb_tab) > 10) {
+        alert("Facebook limit reached!");
+    }
+    if ((gm_visit_count + gm_tab) > 10) {
+        alert("Gmail limit reached!");
+    }
+}
+
